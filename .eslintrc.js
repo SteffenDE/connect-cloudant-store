168ef7a59cbdf0516fee99931decdaaf0f9457c5
steffend <steffen@deusch.me>
2017-10-16 12:45:13 +0200

    initial

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,84 +1,39 @@
+// http://eslint.org/docs/user-guide/configuring
+
 module.exports = {
-    'env': {
-        'node': true,
-        'es6': true,
-        'browser': false
-    },
-    'extends': 'eslint:recommended',
-    'parserOptions': {
-        'sourceType': 'module'
-    },
-    "globals": {
-        'define': false,
-        'require': false
-    },
-    'rules': {
-        'semi': 'error',
-        'eol-last': 'error',
-        'wrap-regex': 'error',
-        'new-parens': 'error',
-        'comma-style': 'error',
-        'semi-spacing': 'error',
-        'no-lonely-if': 'error',
-        'comma-spacing': 'error',
-        'block-spacing': 'error',
-        'no-spaced-func': 'error',
-        'spaced-comment': 'error',
-        'space-in-parens': 'error',
-        'space-infix-ops': 'error',
-        'keyword-spacing': 'error',
-        'no-inline-comments': 'error',
-        'no-trailing-spaces': 'error',
-        'space-before-blocks': 'error',
-        'array-bracket-spacing': 'error',
-        'no-multiple-empty-lines': 'error',
-        'no-mixed-spaces-and-tabs': 'error',
-        'computed-property-spacing': 'error',
-        'no-whitespace-before-property': 'error',
-        'space-before-function-paren': [
-            'error',
-            'never'
-        ],
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'object-curly-spacing': [
-            'error',
-            'never'
-        ],
-        'max-len': [
-            'error',
-            120,
-            4
-        ],
-        'brace-style': [
-            'error',
-            '1tbs',
-            {
-                'allowSingleLine': true
-            }
-        ],
-        'key-spacing': [
-            'error',
-            {
-                'beforeColon': false,
-                'afterColon': true
-            }
-        ],
-        'lines-around-comment': [
-            'error',
-            {
-                'beforeBlockComment': true
-            }
-        ]
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  'settings': {
+    'import/resolver': {
+      'webpack': {
+        'config': 'build/webpack.base.conf.js'
+      }
     }
-};
+  },
+  // add your custom rules here
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'quotes': ["error", "double"],
+    "semi": [2, "always"],
+    "space-before-function-paren": ["error", "never"],
+    "brace-style": ["warn", "stroustrup"],
+    "indent": ["error", 2]
+  }
+}
